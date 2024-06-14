e586824653caf20e32ff16fc7b72d06d36a4dfc6
shivamPrakash9 <shivamkumarseth6@gmail.com>
2024-06-14 07:56:10 +0000

    shivam

---
+++ Vortex/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const moreBtn = document.getElementById('more-btn');
+    
+    moreBtn.addEventListener('click', function() {
+        alert('You clicked the MORE button!');
+        // Here you can add more functionality, like opening a modal or navigating to another page
+    });
+});
